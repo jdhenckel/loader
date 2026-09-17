26052b83539f66c266af2435b350213ad2c3fbf0
John Henckel <jdhenckel@gmail.com>
2026-09-16 21:10:47 -0500

    switch to RAPIER 2D

--- skid_loader.gd
+++ skid_loader.gd
@@ -16,27 +16,16 @@ static func create(pos=null) -> SkidLoader:
 	return skid
 
 
-#static func change_angle(hinge:PinJoint2D, angle:float):
-	#hinge.angular_limit_lower += angle
-	#hinge.angular_limit_upper += angle
-	#hinge.motor_target_velocity = angle * 10
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	drive_hinge($Chassis/Arm1, angle1)
 
 
 func drive_hinge(arm:RigidBody2D, new_angle:float):
-	var h = arm.get_node("Hinge")
-	var d = h.get_node("Drive")
-	var r = Vector2.from_angle(new_angle + PI/2)
-	print(d.position, ' ',new_angle,arm.position)
-	d.position = r * 56
-	arm.position = r * 24 + Vector2(40,0)
-	arm.rotation = new_angle + PI/2
-	
-	
+	var h : RapierPinJoint2D = arm.get_node("Hinge")
+	h.motor_position_target_angle = new_angle
+	print(new_angle)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -45,14 +34,13 @@ func _process(delta: float) -> void:
 func input_key(ch: String, pressed:bool):
 	if ch == 'A': drive = -1 * int(pressed)
 	if ch == 'S': drive = 1 * int(pressed)
-	if ch == 'U': angle1 += .1
-	if ch == 'J': angle1 -= .1
+	if ch == 'U': if angle1<3: angle1 += .1
+	if ch == 'J': if angle1>-1: angle1 -= .1
 
 
 func _physics_process(delta: float) -> void:
 	var b = 10000
 	if drive:
-		print(drive)
 		$Chassis/RearWheel.apply_torque(drive * b)
 		$Chassis/FrontWheel.apply_torque(drive * b)
 	drive_hinge($Chassis/Arm1, angle1)
